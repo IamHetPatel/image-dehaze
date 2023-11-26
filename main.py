ee09f6df714750bd289030783a9a1eb300189102
dharmikjethva30 <dharmikjethva30>
2023-11-26 21:47:14 +0530

    added image resizer

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def select_image():
 
     return file_path
 
+def resize_image(image, max_width=800, max_height=600):
+    height, width = image.shape[:2]
+
+    # Calculate the ratio to maintain the aspect ratio
+    ratio = min(max_width / width, max_height / height)
+
+    # Resize the image
+    resized_image = cv2.resize(image, (int(width * ratio), int(height * ratio)))
+
+    return resized_image
+
 # Select an image file interactively
 image_path = select_image()
 
@@ -18,12 +29,18 @@ if image_path:
     # Read the selected image
     hazy_image = cv2.imread(image_path)
 
+    # Resize the image to fit in the window
+    resized_hazy_image = resize_image(hazy_image)
+
     # Perform dehazing
-    HazeCorrectedImg, HazeMap = dehazer.remove_haze(hazy_image)
+    HazeCorrectedImg, HazeMap = dehazer.remove_haze(resized_hazy_image)
+
+    # Resize the dehazed image to fit in the window
+    resized_dehazed_image = resize_image(HazeCorrectedImg)
 
     # Display the images
-    cv2.imshow('Hazy Image', hazy_image)
-    cv2.imshow('Dehazed Image', HazeCorrectedImg)
+    cv2.imshow('Hazy Image', resized_hazy_image)
+    cv2.imshow('Dehazed Image', resized_dehazed_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
